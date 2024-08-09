7e0a9aadccb5b896444cfadcd01a953cbce41dfe
Radek Hubner <rhubner@users.noreply.github.com>
2024-08-09 11:43:24 +0400

    Fix db.get with java ByteBuffer.

--- java/src/main/java/org/rocksdb/RocksDB.java
+++ java/src/main/java/org/rocksdb/RocksDB.java
@@ -1354,7 +1354,7 @@ public class RocksDB extends RocksObject {
           getDirect(nativeHandle_, opt.nativeHandle_, key, key.position(), key.remaining(), value,
               value.position(), value.remaining(), columnFamilyHandle.nativeHandle_);
       if (result != NOT_FOUND) {
-        value.limit(Math.min(value.limit(), value.position() + result));
+        value.position(Math.min(value.limit(), value.position() + result));
       }
       key.position(key.limit());
       return result;
@@ -1363,7 +1363,7 @@ public class RocksDB extends RocksObject {
           key.arrayOffset() + key.position(), key.remaining(), value.array(),
           value.arrayOffset() + value.position(), value.remaining());
       if (result != NOT_FOUND) {
-        value.limit(Math.min(value.limit(), value.position() + result));
+        value.position(Math.min(value.limit(), value.position() + result));
       }
       key.position(key.limit());
       return result;
